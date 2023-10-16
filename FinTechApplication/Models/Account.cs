3b325fd12db7406f070947d1304f7475b7864b9b
Adetunji Temidayo <adetunjitemidayo26@gmail.com>
2023-10-16 09:26:12 +0100

    Add project files.

---
+++ FinTechApplication/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace FinTechApplication.Models
+{
+    public class Account
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string? AccountNumber { get; set; }
+        public AccountType AccountType { get; set; }
+
+        public decimal AccountBalance { get; set; }
+        public AppUser? User { get; set; }
+    }
+
+    public enum AccountType
+    {
+        Checking,
+        Savings,
+        Investment
+    }
+
+}
