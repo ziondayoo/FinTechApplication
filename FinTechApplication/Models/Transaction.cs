6051e588209c55f6ca754be12dc36d5907f9746b
Adetunji Temidayo <adetunjitemidayo26@gmail.com>
2023-10-16 09:32:05 +0100

    Added Models

---
+++ FinTechApplication/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace FinTechApplication.Models
+{
+    public class Transaction
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public decimal Amount { get; set; }
+        public TransactionType TransactionType { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
+    }
+
+
+}
